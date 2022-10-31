8f5d7a4381b5724de03e1020d5ec04f413278a15
Abigail Queen <107533064+Abi-Queen@users.noreply.github.com>
2022-10-31 05:21:40 -0700

    changed db name to match .env

--- config/connection.js
+++ config/connection.js
@@ -8,18 +8,15 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD,
   {
     host: 'localhost',
+    // host: '127.0.0.1',
     dialect: 'mysql',
-    port: 3301
+    dialectOptions: {
+      decimalNumbers: true
+    },
+    // port: 3301
   }
 )
 
   // ? new Sequelize(process.env.JAWSDB_URL)
-  // : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-  //     host: 'localhost',
-  //     dialect: 'mysql',
-  //     dialectOptions: {
-  //       decimalNumbers: true,
-  //     },
-  //   });
 
 module.exports = sequelize;
